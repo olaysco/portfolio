9f511af5dfa2d718fa03fdcda9f7b6cf01df04b9
olaysco <olayiwolaodunsi@gmail.com>
2020-02-20 00:27:32 +0100

    Update: auto remove construction alert

--- components/ConstructionAlert.vue
+++ components/ConstructionAlert.vue
@@ -1,14 +1,10 @@
 <template>
-  <div>
-<b-alert
-      :show="true"
-      dismissible
-      variant="warning"
-      @dismissed="countDown=0"
-      @dismiss-count-down="(val)=>countDown = val"
-    >
-      Site is still undergoing construction
-    </b-alert>
+  <div v-if="countDown>1">
+    <div class="alert alert-warning alert-dismissible" ref="alert" role="alert">Site is still undergoing construction
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
   </div>
 
 </template>
@@ -16,8 +12,13 @@
 export default {
   data(){
     return {
-      countDown : 5
+      countDown : 4
     }
+  },
+  mounted(){
+    setTimeout(()=>{
+      this.countDown = 0;
+    }, (this.countDown*1000))
   }
 }
 </script>
